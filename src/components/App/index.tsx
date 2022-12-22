9eedead69369a70d312e8d4aa774999d63e80bf1
蒲立望60263 <liwang.pu@going-link.com>
2022-12-22 17:34:22 +0800

    添加导航页面改造

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,39 +1,23 @@
-import './index.scss';
-import { NavLink, Outlet } from "react-router-dom";
-import { memo } from 'react';
+import styles from './index.module.scss';
+import { memo, useMemo } from 'react';
+import SimpleNavsPage, { INavItem } from '../SimpleNavsPage';
 
-const routes: { title: string; path: string }[] = [
-  {
-    title: 'Home',
-    path: '/app/home',
-  },
-  {
-    title: 'Test',
-    path: '/app/test',
-  }
-];
+const App: React.FC = memo(() => {
 
-const RouterLinks = routes.map(r => (
-  <NavLink key={r.title} to={r.path} className={({ isActive }) => isActive ? "item actived" : "item"}>
-    <p>{r.title}</p>
-  </NavLink >
-));
-
-const App: React.FC = memo((props) => {
+  const routes = useMemo<Array<INavItem>>(() => ([
+    {
+      title: 'Home',
+      path: '/app/home',
+    },
+    {
+      title: 'Test',
+      path: '/app/test',
+    }
+  ]), []);
 
   return (
-    <div className='tutorial-app'>
-      <div className="navs">
-        <div className="header">
-          <p className='title'>学习里程</p>
-        </div>
-        <div className="content">
-          {RouterLinks}
-        </div>
-      </div>
-      <div className="page">
-        <Outlet />
-      </div>
+    <div className={styles['tutorial-app']}>
+      <SimpleNavsPage title='学习里程' routes={routes} />
     </div>
   );
 });
